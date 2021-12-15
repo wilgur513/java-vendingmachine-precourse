e651cf300aa1dccfe2ba58e642e0c433d9238d1b
wilgur513 <wilgur513@naver.com>
2021-12-15 22:51:32 +0900

    refactor: extract class for generate coins

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,35 +1,12 @@
 package vendingmachine;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.Arrays;
 import vendingmachine.view.InputView;
 
 public class Application {
 	public static void main(String[] args) {
 		String money = inputMoney();
-		Coins coins = generateCoins(money);
+		Coins coins = new CoinsGenerator().generate(money);
 		System.out.println(coins);
-	}
-
-	private static Coins generateCoins(String money) {
-		int moneyValue = Integer.parseInt(money);
-		Coins coins = new Coins();
-
-		while (moneyValue != 0) {
-			Coin coin = pickRandomCoin();
-
-			if(coin.isLessAndEqual(moneyValue)) {
-				coins.put(coin);
-				moneyValue -= coin.getAmount();
-			}
-		}
-
-		return coins;
-	}
-
-	private static Coin pickRandomCoin() {
-		Integer value = Randoms.pickNumberInList(Arrays.asList(500, 100, 50, 10));
-		return Coin.valueOf(value);
 	}
 
 	private static String inputMoney() {
---
+++ src/main/java/vendingmachine/CoinsGenerator.java
@@ -0,0 +1,27 @@
+package vendingmachine;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
+
+public class CoinsGenerator {
+	public Coins generate(String money) {
+		int moneyValue = Integer.parseInt(money);
+		Coins coins = new Coins();
+
+		while (moneyValue != 0) {
+			Coin coin = pickRandomCoin();
+
+			if(coin.isLessAndEqual(moneyValue)) {
+				coins.put(coin);
+				moneyValue -= coin.getAmount();
+			}
+		}
+
+		return coins;
+	}
+
+	private Coin pickRandomCoin() {
+		Integer value = Randoms.pickNumberInList(Arrays.asList(500, 100, 50, 10));
+		return Coin.valueOf(value);
+	}
+}

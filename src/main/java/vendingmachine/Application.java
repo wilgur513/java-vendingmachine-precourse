0ca606291b6c1ad591efe1953e417a007ee63b7f
wilgur513 <wilgur513@naver.com>
2021-12-15 23:21:51 +0900

    feat: extract vending machine controller

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -6,30 +6,7 @@ import vendingmachine.view.OutputView;
 
 public class Application {
 	public static void main(String[] args) {
-		String coinsAmount = inputMoney("자판기가 보유하고 있는 금액을 입력해 주세요.");
-		Coins coins = new CoinsGenerator().generate(coinsAmount);
-		OutputView.printChangeCoins(coins);
-		List<Item> itemList = inputItemList();
-		String inputMoney = inputMoney("투입 금액을 입력해 주세요.");
-	}
-
-	private static String inputMoney(String message) {
-		try {
-			return InputView.inputMoney(message);
-		} catch (IllegalArgumentException e) {
-			System.out.println("[ERROR] " + e.getMessage());
-			System.out.println();
-			return inputMoney(message);
-		}
-	}
-
-	private static List<Item> inputItemList() {
-		try {
-			return InputView.inputItemList();
-		} catch (IllegalArgumentException e) {
-			System.out.println("[ERROR] " + e.getMessage());
-			System.out.println();
-			return inputItemList();
-		}
+		VendingMachineController controller = new VendingMachineController();
+		controller.service();
 	}
 }
---
+++ src/main/java/vendingmachine/VendingMachineController.java
@@ -0,0 +1,35 @@
+package vendingmachine;
+
+import java.util.List;
+import vendingmachine.view.InputView;
+import vendingmachine.view.OutputView;
+
+public class VendingMachineController {
+	public void service() {
+		String coinsAmount = inputMoney("자판기가 보유하고 있는 금액을 입력해 주세요.");
+		Coins coins = new CoinsGenerator().generate(coinsAmount);
+		OutputView.printChangeCoins(coins);
+		List<Item> itemList = inputItemList();
+		String inputMoney = inputMoney("투입 금액을 입력해 주세요.");
+	}
+
+	private static String inputMoney(String message) {
+		try {
+			return InputView.inputMoney(message);
+		} catch (IllegalArgumentException e) {
+			System.out.println("[ERROR] " + e.getMessage());
+			System.out.println();
+			return inputMoney(message);
+		}
+	}
+
+	private static List<Item> inputItemList() {
+		try {
+			return InputView.inputItemList();
+		} catch (IllegalArgumentException e) {
+			System.out.println("[ERROR] " + e.getMessage());
+			System.out.println();
+			return inputItemList();
+		}
+	}
+}

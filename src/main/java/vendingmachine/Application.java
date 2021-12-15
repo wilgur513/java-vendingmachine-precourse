7bec52bd2d607fd41f1257121b2d96d640d750c0
wilgur513 <wilgur513@naver.com>
2021-12-15 22:38:55 +0900

    feat: input valid number money

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -3,8 +3,24 @@ package vendingmachine;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
-        String money = Console.readLine();
-    }
+	public static void main(String[] args) {
+		try {
+			String money = inputMoney();
+		} catch(IllegalArgumentException e) {
+			System.out.println("[ERROR] " + e.getMessage());
+			System.out.println();
+		}
+	}
+
+	private static String inputMoney() {
+		System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
+		String money = Console.readLine();
+		System.out.println();
+
+		if (!money.matches("[0-9]+")) {
+			throw new IllegalArgumentException("금액은 숫자여야 합니다.");
+		}
+
+		return money;
+	}
 }
